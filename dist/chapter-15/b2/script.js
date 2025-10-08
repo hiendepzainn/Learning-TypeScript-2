9122c8f05907ffe0db0499f95363e2ae84968fce
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-10-08 08:08:30 +0700

    done: part 4 of Chapter 15

--- dist/chapter-15/b2/script.js
+++ dist/chapter-15/b2/script.js
@@ -2,14 +2,63 @@ import { randomNumber } from "./utils.js";
 const clearInput = () => {
     inputTodo.value = "";
 };
-const reloadPage = () => {
-    window.location.reload();
-};
 const inputTodo = document.getElementById("todoInput");
 const saveButton = document.getElementById("saveChangeButton");
 const tableElement = document.getElementById("bodyTable");
 if (inputTodo === null || saveButton === null || tableElement === null) {
     throw new Error("Cannot find Elements");
+}
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+const toastTrigger2 = document.getElementById("liveToastBtn2");
+const toastLiveExample2 = document.getElementById("liveToast2");
+if (toastTrigger) {
+    //@ts-ignore
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastTrigger.addEventListener("click", () => {
+        toastBootstrap.show();
+    });
+}
+if (toastTrigger2) {
+    //@ts-ignore
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample2);
+    toastTrigger2.addEventListener("click", () => {
+        toastBootstrap.show();
+    });
+}
+const deleteTodoLocalStorage = (id) => {
+    const todoListJSON2 = localStorage.getItem("data");
+    const todoListJS = JSON.parse(todoListJSON2);
+    const newList = todoListJS.filter((item) => item.id !== id);
+    localStorage.setItem("data", JSON.stringify(newList));
+};
+//Event delete
+const assignDeleteToButton = () => {
+    const deleteList = document.querySelectorAll("td button");
+    deleteList.forEach((item) => {
+        item.addEventListener("click", () => {
+            const idDelete = Number(item.getAttribute("row-id"));
+            deleteTodoLocalStorage(idDelete);
+            const deleteRow = item.parentNode.parentNode;
+            console.log("delete row:", deleteRow);
+            tableElement.removeChild(deleteRow);
+            toastTrigger.click();
+        });
+    });
+};
+//Display data
+const todoListJSON = localStorage.getItem("data");
+if (todoListJSON) {
+    const todoListJS = JSON.parse(todoListJSON);
+    todoListJS.forEach((item, index) => {
+        tableElement.innerHTML += `
+        <tr>
+          <td>${item.id}</td>
+          <td>${item.todo}</td>
+          <td><button class="btn btn-danger" row-id="${item.id}">Delete</button></td>
+        </tr>
+    `;
+    });
 }
 saveButton.addEventListener("click", () => {
     const newID = randomNumber();
@@ -28,34 +77,14 @@ saveButton.addEventListener("click", () => {
         localStorage.setItem("data", JSON.stringify([newTodo]));
     }
     clearInput();
-    reloadPage();
-});
-//Display data
-const todoListJSON = localStorage.getItem("data");
-if (todoListJSON) {
-    const todoListJS = JSON.parse(todoListJSON);
-    todoListJS.forEach((item, index) => {
-        tableElement.innerHTML += `
+    tableElement.innerHTML += `
         <tr>
-          <th scope="row">${index + 1}</th>
-          <td>${item.id}</td>
-          <td>${item.todo}</td>
-          <td><button class="btn btn-danger" row-id="${item.id}">Delete</button></td>
+          <td>${newTodo.id}</td>
+          <td>${newTodo.todo}</td>
+          <td><button class="btn btn-danger" row-id="${newTodo.id}">Delete</button></td>
         </tr>
     `;
-    });
-}
-const deleteTodo = (id) => {
-    const todoListJS = JSON.parse(todoListJSON);
-    const newList = todoListJS.filter((item) => item.id !== id);
-    localStorage.setItem("data", JSON.stringify(newList));
-    reloadPage();
-};
-//Event delete
-const deleteList = document.querySelectorAll("td button");
-deleteList.forEach((item) => {
-    item.addEventListener("click", () => {
-        const idDelete = Number(item.getAttribute("row-id"));
-        deleteTodo(idDelete);
-    });
+    assignDeleteToButton();
+    toastTrigger2.click();
 });
+assignDeleteToButton();
--- src/chapter-15/b2/script.ts
+++ src/chapter-15/b2/script.ts
@@ -4,10 +4,6 @@ const clearInput = () => {
   inputTodo!.value = "";
 };
 
-const reloadPage = () => {
-  window.location.reload();
-};
-
 const inputTodo = document.getElementById(
   "todoInput"
 ) as HTMLInputElement | null;
@@ -22,9 +18,71 @@ if (inputTodo === null || saveButton === null || tableElement === null) {
   throw new Error("Cannot find Elements");
 }
 
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+const toastTrigger2 = document.getElementById("liveToastBtn2");
+const toastLiveExample2 = document.getElementById("liveToast2");
+
+if (toastTrigger) {
+  //@ts-ignore
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toastTrigger.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+
+if (toastTrigger2) {
+  //@ts-ignore
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample2);
+  toastTrigger2.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+
 interface ITodo {
   id: number;
   todo: string;
+}
+
+const deleteTodoLocalStorage = (id: number) => {
+  const todoListJSON2: string | null = localStorage.getItem("data");
+
+  const todoListJS: ITodo[] = JSON.parse(todoListJSON2!) as ITodo[];
+  const newList: ITodo[] = todoListJS.filter((item) => item.id !== id);
+
+  localStorage.setItem("data", JSON.stringify(newList));
+};
+
+//Event delete
+const assignDeleteToButton = () => {
+  const deleteList = document.querySelectorAll("td button");
+  deleteList.forEach((item) => {
+    item.addEventListener("click", () => {
+      const idDelete: number = Number(item.getAttribute("row-id"));
+      deleteTodoLocalStorage(idDelete);
+
+      const deleteRow = item.parentNode!.parentNode!;
+      console.log("delete row:", deleteRow);
+
+      tableElement.removeChild(deleteRow);
+      toastTrigger!.click();
+    });
+  });
+};
+
+//Display data
+const todoListJSON: string | null = localStorage.getItem("data");
+if (todoListJSON) {
+  const todoListJS: ITodo[] = JSON.parse(todoListJSON) as ITodo[];
+  todoListJS.forEach((item, index) => {
+    tableElement.innerHTML += `
+        <tr>
+          <td>${item.id}</td>
+          <td>${item.todo}</td>
+          <td><button class="btn btn-danger" row-id="${item.id}">Delete</button></td>
+        </tr>
+    `;
+  });
 }
 
 saveButton.addEventListener("click", () => {
@@ -46,42 +104,15 @@ saveButton.addEventListener("click", () => {
   }
 
   clearInput();
-  reloadPage();
-});
-
-//Display data
-const todoListJSON: string | null = localStorage.getItem("data");
-if (todoListJSON) {
-  const todoListJS: ITodo[] = JSON.parse(todoListJSON) as ITodo[];
-  todoListJS.forEach((item, index) => {
-    tableElement.innerHTML += `
+  tableElement.innerHTML += `
         <tr>
-          <th scope="row">${index + 1}</th>
-          <td>${item.id}</td>
-          <td>${item.todo}</td>
-          <td><button class="btn btn-danger" row-id="${
-            item.id
-          }">Delete</button></td>
+          <td>${newTodo.id}</td>
+          <td>${newTodo.todo}</td>
+          <td><button class="btn btn-danger" row-id="${newTodo.id}">Delete</button></td>
         </tr>
     `;
-  });
-}
-
-const deleteTodo = (id: number) => {
-  const todoListJS: ITodo[] = JSON.parse(todoListJSON!) as ITodo[];
-  const newList: ITodo[] = todoListJS.filter((item) => item.id !== id);
-
-  localStorage.setItem("data", JSON.stringify(newList));
-
-  reloadPage();
-};
-
-//Event delete
-const deleteList = document.querySelectorAll("td button");
-deleteList.forEach((item) => {
-  item.addEventListener("click", () => {
-    const idDelete: number = Number(item.getAttribute("row-id"));
-
-    deleteTodo(idDelete);
-  });
+  assignDeleteToButton();
+  toastTrigger2!.click();
 });
+
+assignDeleteToButton();
