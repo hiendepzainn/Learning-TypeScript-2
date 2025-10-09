d687bd5cf9681a5b218c917990abe2b95f7a4580
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-10-09 15:38:48 +0700

    done: part 2+3+4 Chapter 17

--- dist/chapter-16/src/blog/blog.js
+++ dist/chapter-16/src/blog/blog.js
@@ -6,7 +6,55 @@ const deleteBlog = async (id) => {
             "Content-Type": "application/json",
         },
     });
+    console.log("Res Delete:", res);
+};
+const createBlog = async (blog) => {
+    const res = await fetch("http://localhost:1410/blogs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(blog),
+    });
+    console.log("Res Create:", res);
+};
+const updateBlog = async (blog) => {
+    const res = await fetch(`http://localhost:1410/blogs/${blog.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(blog),
+    });
     console.log(res);
+};
+const assignEditToButton = () => {
+    const editButtonList = document.querySelectorAll(".edit-button");
+    editButtonList.forEach((item) => {
+        item.addEventListener("click", () => {
+            const listInput = document.querySelectorAll("input");
+            listInput[3].value = item.getAttribute("id-row");
+            listInput[4].value = item.getAttribute("title-row");
+            listInput[5].value = item.getAttribute("description-row");
+        });
+    });
+};
+const assignUpdateToButton = () => {
+    const updateButton = document.getElementById("updateButton");
+    updateButton.addEventListener("click", async () => {
+        const listInput = document.querySelectorAll("input");
+        const id = listInput[3].value;
+        const title = listInput[4].value;
+        const description = listInput[5].value;
+        const newBlog = {
+            id,
+            title,
+            description,
+        };
+        await updateBlog(newBlog);
+        await fetchAndSet();
+        toastTrigger3.click();
+    });
 };
 const assignDeleteToButton = () => {
     const deleteButtonList = document.querySelectorAll(".btn-danger");
@@ -17,7 +65,25 @@ const assignDeleteToButton = () => {
             await deleteBlog(idDelete);
             //reFetch
             await fetchAndSet();
+            toastTrigger2.click();
         });
+    });
+};
+const assignCreateNewBlog = () => {
+    const saveButton = document.getElementById("saveButton");
+    saveButton.addEventListener("click", async () => {
+        const listInput = document.querySelectorAll("input");
+        const newBlog = {
+            id: listInput[0].value,
+            title: listInput[1].value,
+            description: listInput[2].value,
+        };
+        await createBlog(newBlog);
+        listInput.forEach((item) => {
+            item.value = "";
+        });
+        await fetchAndSet();
+        toastTrigger1.click();
     });
 };
 const fetchAndSet = async () => {
@@ -32,7 +98,17 @@ const fetchAndSet = async () => {
             <td>${item.title}</td>
             <td>${item.description}</td>
             <td>
-              <button class="btn btn-warning me-2">Edit</button>
+              <button
+              type="button"
+              class="edit-button btn btn-warning"
+              id-row="${item.id}"
+              title-row="${item.title}"
+              description-row="${item.description}"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop2"
+              >
+              Edit
+              </button>
               <button class="btn btn-danger">Delete</button>
             </td>
         </tr>
@@ -40,8 +116,38 @@ const fetchAndSet = async () => {
     });
     tableElement.innerHTML = htmlTable;
     assignDeleteToButton();
+    assignEditToButton();
 };
 const mainExecute = async () => {
     await fetchAndSet();
+    assignCreateNewBlog();
+    assignUpdateToButton();
 };
+const toastTrigger1 = document.getElementById("liveToastBtn-create");
+const toastLiveExample1 = document.getElementById("liveToast-create");
+if (toastTrigger1) {
+    //@ts-ignore
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample1);
+    toastTrigger1.addEventListener("click", () => {
+        toastBootstrap.show();
+    });
+}
+const toastTrigger2 = document.getElementById("liveToastBtn-delete");
+const toastLiveExample2 = document.getElementById("liveToast-delete");
+if (toastTrigger2) {
+    //@ts-ignore
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample2);
+    toastTrigger2.addEventListener("click", () => {
+        toastBootstrap.show();
+    });
+}
+const toastTrigger3 = document.getElementById("liveToastBtn-update");
+const toastLiveExample3 = document.getElementById("liveToast-update");
+if (toastTrigger3) {
+    //@ts-ignore
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample3);
+    toastTrigger3.addEventListener("click", () => {
+        toastBootstrap.show();
+    });
+}
 mainExecute();
--- src/chapter-16/src/blog/blog.ts
+++ src/chapter-16/src/blog/blog.ts
@@ -11,7 +11,64 @@ const deleteBlog = async (id: string) => {
       "Content-Type": "application/json",
     },
   });
+  console.log("Res Delete:", res);
+};
+
+const createBlog = async (blog: IBlog) => {
+  const res = await fetch("http://localhost:1410/blogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(blog),
+  });
+  console.log("Res Create:", res);
+};
+
+const updateBlog = async (blog: IBlog) => {
+  const res = await fetch(`http://localhost:1410/blogs/${blog.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(blog),
+  });
   console.log(res);
+};
+
+const assignEditToButton = () => {
+  const editButtonList = document.querySelectorAll(".edit-button");
+  editButtonList.forEach((item) => {
+    item.addEventListener("click", () => {
+      const listInput = document.querySelectorAll("input");
+
+      listInput[3].value = item.getAttribute("id-row")!;
+      listInput[4].value = item.getAttribute("title-row")!;
+      listInput[5].value = item.getAttribute("description-row")!;
+    });
+  });
+};
+
+const assignUpdateToButton = () => {
+  const updateButton = document.getElementById("updateButton");
+  updateButton!.addEventListener("click", async () => {
+    const listInput = document.querySelectorAll("input");
+    const id = listInput[3].value;
+    const title = listInput[4].value;
+    const description = listInput[5].value;
+
+    const newBlog: IBlog = {
+      id,
+      title,
+      description,
+    };
+
+    await updateBlog(newBlog);
+
+    await fetchAndSet();
+
+    toastTrigger3!.click();
+  });
 };
 
 const assignDeleteToButton = () => {
@@ -26,7 +83,29 @@ const assignDeleteToButton = () => {
 
       //reFetch
       await fetchAndSet();
+
+      toastTrigger2!.click();
     });
+  });
+};
+
+const assignCreateNewBlog = () => {
+  const saveButton = document.getElementById("saveButton") as HTMLButtonElement;
+  saveButton.addEventListener("click", async () => {
+    const listInput = document.querySelectorAll("input");
+    const newBlog: IBlog = {
+      id: listInput[0].value,
+      title: listInput[1].value,
+      description: listInput[2].value,
+    };
+    await createBlog(newBlog);
+
+    listInput.forEach((item) => {
+      item.value = "";
+    });
+
+    await fetchAndSet();
+    toastTrigger1!.click();
   });
 };
 
@@ -46,7 +125,17 @@ const fetchAndSet = async () => {
             <td>${item.title}</td>
             <td>${item.description}</td>
             <td>
-              <button class="btn btn-warning me-2">Edit</button>
+              <button
+              type="button"
+              class="edit-button btn btn-warning"
+              id-row="${item.id}"
+              title-row="${item.title}"
+              description-row="${item.description}"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop2"
+              >
+              Edit
+              </button>
               <button class="btn btn-danger">Delete</button>
             </td>
         </tr>
@@ -54,10 +143,43 @@ const fetchAndSet = async () => {
   });
   tableElement.innerHTML = htmlTable;
   assignDeleteToButton();
+  assignEditToButton();
 };
 
 const mainExecute = async () => {
   await fetchAndSet();
+  assignCreateNewBlog();
+  assignUpdateToButton();
 };
+
+const toastTrigger1 = document.getElementById("liveToastBtn-create");
+const toastLiveExample1 = document.getElementById("liveToast-create");
+if (toastTrigger1) {
+  //@ts-ignore
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample1);
+  toastTrigger1.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+
+const toastTrigger2 = document.getElementById("liveToastBtn-delete");
+const toastLiveExample2 = document.getElementById("liveToast-delete");
+if (toastTrigger2) {
+  //@ts-ignore
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample2);
+  toastTrigger2.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
+
+const toastTrigger3 = document.getElementById("liveToastBtn-update");
+const toastLiveExample3 = document.getElementById("liveToast-update");
+if (toastTrigger3) {
+  //@ts-ignore
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample3);
+  toastTrigger3.addEventListener("click", () => {
+    toastBootstrap.show();
+  });
+}
 
 mainExecute();
